ac6df18a496eb957c56a92112472f60d348edf2d
Elvira-g <elvira_27@mail.ru>
2022-08-15 19:22:59 +0300

    feat: add structure

---
+++ src/sprint/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from './components/Card';
+import './style.css';
+
+export default function Main() {
+    return (
+        <div className='sprint-wrapper'>
+            <Card />
+        </div>
+    )
+}
